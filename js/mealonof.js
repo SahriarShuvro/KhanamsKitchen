87628227a36e5904669edbb53359d14c17132e44
SahriarShuvro <80584229+SahriarShuvro@users.noreply.github.com>
2022-08-02 10:20:51 +0600

    Add files via upload

--- js/mealonof.js
+++ js/mealonof.js
@@ -1,52 +1,27 @@
 {
-    {
-        let disableCheck = document.querySelectorAll(".check-dis");
+    let disableCheckL = document.querySelectorAll(".check-dis");
+    let disableCheckD = document.querySelectorAll(".check-d");
 
-        const d = new Date("July 30, 2022 21:00:00");
-        let day = d.getDate();
-        let hour = d.getHours();
-        let minute = d.getMinutes();
-        for (let i = 0; i < 1; i++) {
-            day++;
-            if (hour > 00) {
-                disableCheck[i].setAttribute("disabled", "disabled");
-            } else if (minute > 30) {
-                disableCheck[i].setAttribute("disabled", "disabled");
+    const d = new Date();
+    let day = d.getDate();
+    let hour = d.getHours();
+    let minute = d.getMinutes();
+    for (let i = 0; i < 1; i++) {
+        day++;
+        if (hour > 00) {
+            disableCheckL[i].setAttribute("disabled", "disabled");
+        }
+    }
+    for (let i = 0; i < 1; i++) {
+        day++;
+        if (hour > 14) {
+            disableCheckD[i].setAttribute("disabled", "disabled");
+        } else if (minute > 30) {
+            disableCheckD[i].setAttribute("disabled", "disabled");
 
-            }
         }
     }
 
-    // Dinner 
-
-    {
-        let disableCheck = document.querySelectorAll(".check-d");
-
-        const d = new Date();
-        let day = d.getDate();
-        let hour = d.getHours();
-        let minute = d.getMinutes();
-        for (let i = 0; i < 1; i++) {
-            day++;
-            if (hour > 14) {
-                disableCheck[i].setAttribute("disabled", "disabled");
-            } else if (minute > 30) {
-                disableCheck[i].setAttribute("disabled", "disabled");
-
-            }
-        }
-    }
-
-    // {
-    //     let disableCheck = document.querySelectorAll(".check-d");
-
-    //     const d = new Date();
-    //     let day = d.getDate();
-    //     for (let i = 0; i < 1; i++) {
-    //         day++;
-    //         disableCheck[i].setAttribute("disabled", "disabled");
-    //     }
-    // }
 }
 
 {
@@ -57,6 +32,20 @@
         let day = d.getDate() - 1;
 
         let dayShow = document.querySelectorAll(".p-d");
+        for (let i = 0; i <= month; i++) {
+            day++
+            dayShow[i].innerHTML = day;
+            if (day > month) {
+                day = 0
+            }
+        }
+    } {
+        let dayOfMonth = [30, 27, 30, 29, 30, 29, 30, 30, 29, 30, 29, 30]
+        let d = new Date();
+        let month = dayOfMonth[d.getMonth()];
+        let day = d.getDate() - 1;
+
+        let dayShow = document.querySelectorAll(".p-dd");
         for (let i = 0; i <= month; i++) {
             day++
             dayShow[i].innerHTML = day;
--- js/mealonof2.js
+++ js/mealonof2.js
@@ -1,17 +1,17 @@
-{
-    {
-        let dayOfMonth = [30, 27, 30, 29, 30, 29, 30, 30, 29, 30, 29, 30]
-        let d = new Date();
-        let month = dayOfMonth[d.getMonth()];
-        let day = d.getDate() - 1;
+ {
+     {
+         let dayOfMonth = [30, 27, 30, 29, 30, 29, 30, 30, 29, 30, 29, 30]
+         let d = new Date();
+         let month = dayOfMonth[d.getMonth()];
+         let day = d.getDate() - 1;
 
-        let dayShow = document.querySelectorAll(".p-dd");
-        for (let i = 0; i <= month; i++) {
-            day++
-            dayShow[i].innerHTML = day;
-            if (day > month) {
-                day = 0
-            }
-        }
-    }
-}
+         let dayShow = document.querySelectorAll(".p-dd");
+         for (let i = 0; i <= month; i++) {
+             day++
+             dayShow[i].innerHTML = day;
+             if (day > month) {
+                 day = 0
+             }
+         }
+     }
+ }
